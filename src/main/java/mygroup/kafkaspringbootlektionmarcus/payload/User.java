cab73612049d752351276ff400c5f547e471d3db
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2023-09-05 12:08:29 +0200

    MongoDB Server storage fixed, it now stores to the server storage primarily. A customID'-parameter was added in the User-class, so that the User can type in its own Custom ID-number, but mongoDB keeps its own unique record id.
    still havent got it to work if server connection fails save local.

--- src/main/java/mygroup/kafkaspringbootlektionmarcus/payload/User.java
+++ src/main/java/mygroup/kafkaspringbootlektionmarcus/payload/User.java
@@ -2,22 +2,21 @@ package mygroup.kafkaspringbootlektionmarcus.payload;
 
 public class User {
 
+    private String customId;
     private String firstName;
     private String lastName;
 
-
+    public String getCustomId() {return customId;}
+    public void setCustomId(String customId) {this.customId = customId;}
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -25,6 +24,7 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
+                "customId='" + customId + '\'' +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 '}';
