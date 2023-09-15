64db2d48da77892db06f2d7489de2d8a78b0489a
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2023-09-15 11:11:34 +0200

    SQL implementation Branch is working, and adding the values correctly to MySQL workbench. If you remember to change the passwords in both MongoDBconfig for mongoDB and application.properties for MySQL, it will work to store data to both databases simultaneously.
    Still havent got it to work if server connection fails save local.

--- src/main/java/mygroup/kafkaspringbootlektionmarcus/KafkaSpringbootLektionMarcusApplication.java
+++ src/main/java/mygroup/kafkaspringbootlektionmarcus/KafkaSpringbootLektionMarcusApplication.java
@@ -8,5 +8,10 @@ public class KafkaSpringbootLektionMarcusApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaSpringbootLektionMarcusApplication.class, args);
+
+        // NOTE: IF run/saving to either MongoDB server, or MySQL local storage isnt working, have you remembered to change the
+        // MongoDB password in the class MongoDBConfig? and
+        // MySQL password in the file application.properties?
+
     }
 }
